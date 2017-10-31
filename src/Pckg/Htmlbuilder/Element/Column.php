e0420cd29c7245e37d945b32390f3bad74b6cb77
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-10-31 12:12:11 +0100

    Use labeled

--- src/Pckg/Htmlbuilder/Element/Column.php
+++ src/Pckg/Htmlbuilder/Element/Column.php
@@ -1,11 +1,12 @@
 <?php namespace Pckg\Htmlbuilder\Element;
 
 use Pckg\Htmlbuilder\Snippet\Buildable;
+use Pckg\Htmlbuilder\Snippet\Labeled;
 
 class Column extends Div
 {
 
-    use Buildable;
+    use Buildable, Labeled;
 
     protected $classes = ['form-column'];
 
