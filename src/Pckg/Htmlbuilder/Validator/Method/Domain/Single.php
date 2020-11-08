304f2290d4188229d80c28baec4ebd446516f406
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-11-08 20:23:16 +0100

    Single subdomain validator

--- src/Pckg/Htmlbuilder/Validator/Method/Domain/Single.php
+++ src/Pckg/Htmlbuilder/Validator/Method/Domain/Single.php
@@ -23,7 +23,7 @@ class Single extends \Pckg\Htmlbuilder\Validator\AbstractValidator implements Va
     public static function isValidDomain($domain)
     {
         return strlen($domain) <= 253
-            && preg_match('/^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/i', $domain);
+            && preg_match('/^(?:[-A-Za-z0-9\.]+)+[A-Za-z]{2,6}$/i', $domain);
     }
 
 }
